b041a9a1ad89696bb597e4a467ff4de30d0b4049
krish virani <viranikrish27@gmail.com>
2025-11-14 19:23:44 +0530

    schema validation done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const wrapAsync = require('./utils/wrapAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -28,6 +29,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
+
+// schema validation middelware
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    let errMsg = error.details.map((el) => el.message).join(",");
+    if(error) {
+        throw new ExpressError(400, errMsg);
+    }
+}
 
 app.get('/', (req, res) => {
     res.send("Hi, I am root");
@@ -52,10 +62,7 @@ app.get('/listings/:id', wrapAsync(async (req, res) => {
 }));
 
 // Add Listing 
-app.post('/listings', wrapAsync(async (req, res, next) => {
-    if(!req.body.listing) {
-        throw new ExpressError(400, "Send Valid Data for listing");
-    }
+app.post('/listings', validateListing, wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -69,10 +76,7 @@ app.get('/listings/:id/edit', wrapAsync(async (req, res) => {
 }));
 
 // update listing
-app.put('/listings/:id', wrapAsync(async (req, res) => {
-    if(!req.body.listing) {
-        throw new ExpressError(400, "Send Valid Data for listing");
-    }
+app.put('/listings/:id', validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect("/listings");
@@ -94,8 +98,8 @@ app.use((req, res, next) => {
 // error handling middelware
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong!" } = err;
-    res.render("error.ejs", {message});
-    
+    res.render("error.ejs", { message });
+
 });
 
 app.listen(8080, () => {
